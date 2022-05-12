c397cff8f8466a1b332498fa666d062f669368a8
ThiagoW21 <ThiagoW21@users.noreply.github.com>
2022-05-12 02:15:18 +0000

    deploy: c62a3520be617714bb6195268ae78341306b45f4

---
+++ precache-manifest.c0ba53cdc81397e85983d30907e24fc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7bc883c8079eb64ec4f0bebf003dba",
+    "url": "/project-trivia/index.html"
+  },
+  {
+    "revision": "4043c8b436123661d934",
+    "url": "/project-trivia/static/css/main.ae990342.chunk.css"
+  },
+  {
+    "revision": "1791b0de744d5fe5b5f6",
+    "url": "/project-trivia/static/js/2.ce14b3ef.chunk.js"
+  },
+  {
+    "revision": "ca6b5a6a129747daa0202cfb1dcfa4c4",
+    "url": "/project-trivia/static/js/2.ce14b3ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4043c8b436123661d934",
+    "url": "/project-trivia/static/js/main.1a64f340.chunk.js"
+  },
+  {
+    "revision": "d661d278f9eee2565a60",
+    "url": "/project-trivia/static/js/runtime-main.f937286e.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/project-trivia/static/media/trivia.466d153e.png"
+  }
+]);
